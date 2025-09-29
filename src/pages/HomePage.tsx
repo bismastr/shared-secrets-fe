0867734e8db32ea5936a4957797c62e5ec167afd
bismastr <bismasatrian@gmail.com>
2025-09-29 23:29:29 +0700

    feat: add card

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
 import { AppLayout } from "@/components/layout/AppLayout";
-import { Button } from "@/components/ui/button";
+import { QuestionCard } from "@/components/QuestionCard";
 
 
 const HomePage = () => {
     return (
         <div>
             <AppLayout>
-                <div className="flex flex-col gap-4 items-center">
-                    <Button variant="default">Utarakan</Button>
+                <div className="flex flex-col gap-4 items-center justify-center h-full">
+                    <QuestionCard question="UTARAKAN UTARAKAN" className=" w-full" />
                     <div>Walau tak semua tanya, datang berserta jawabnya</div>
                 </div>
             </AppLayout>
